bebf52214a48ef22f0a77f247545dc7ca29f8ef7
Jeena Chemparathy <jeena@jeenas-mbp.lan>
2019-02-07 16:34:32 -0800

    Need to do array and obj

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,28 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+// to remove quotes
+  function removeQuotes(str) {
+    var endIndex = str.length - 1;
+    if (str[0] === '"' && str[endIndex] === '"') {
+      return str.slice(1, endIndex);
+    }
+    return str;
+  }
+  
+  if(typeof json === 'null') {
+    return null;
+  } else if (json === 'true') {
+    return true;
+  } else if (json === 'false') {
+    return false;
+  } else if(typeof json === 'string') {
+    return  removeQuotes(json);
+  } else if(typeof Number(json) === 'number') {
+    return Number(json);
+  } else if(Array.isArray(json)) {
+     for (var i = 0; i < json.length; i++) {
+       removeQuotes(parseJSON(json[i]))
+     }
+  }
 };
